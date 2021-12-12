e6e0e767fcac260df13b39e7750f5d9e07c9bed7
Mohamed Eid <medoeid50@gmail.com>
2021-12-12 12:07:54 +0200

    wip

---
+++ core/App.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bluex\Framework;
+
+class App
+{
+    public function __construct()
+    {
+    }
+
+    public function run()
+    {
+        echo "hello world";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+use Bluex\Framework\App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new App();
+
+$app->run();
